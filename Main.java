65e74531c38c38c44bad644bd0e88d375b3bb288
169724 <126686272+169724@users.noreply.github.com>
2023-03-09 17:48:11 +0000

    sukces

--- Main.java
+++ Main.java
@@ -3,31 +3,52 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
-    
+
     while (true) {
-      System.out.println("Kalkulator!!!!");
+      System.out.println("==============================");
+      System.out.println("        [!KALKULATOR!]        ");
+      System.out.println("==============================");
       System.out.println("1. Suma 2. Różnica 3. Iloczyn 4. Iloraz");
+        System.out.println("");
       int program = s.nextInt();
       if (program < 0 || program > 5) {
-        System.out.println("WYBIERZ PROGRAM OD 1 DO 4!!!!");
-        System.out.println("=============================");
-        System.out.println("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_");
-        System.out.println("=============================");
-        continue;
-      }
 
+      System.out.println("==============================");
+      System.out.println("        [ERROR] [ERROR]       ");
+      System.out.println("==============================");
+        System.out.println("");
+      System.out.println("--------------------------------------------------------------");
+        System.out.println("");
+      continue;
+      }
+        System.out.println("");
       System.out.println("|Podaj Liczbę a: ");
+        System.out.println("");
       Float a = s.nextFloat();
+        System.out.println("");
       System.out.println("||Podaj Liczbę b: ");
+        System.out.println("");
       float b = s.nextFloat();
+        System.out.println("");
       switch (program) {
         case 1:
+          System.out.println("Wynik: "+ (a+b));
           break;
         case 2:
+          System.out.println("Wynik: "+ (a-b));
           break;
         case 3:
+          System.out.println("Wynik: "+ (a*b));
           break;
         case 4:
+          if(b==0){
+          System.out.println("NIE DZIEL PRZEZ ZERO GAMONIU");
+            System.out.println("");
+            System.out.println("");
+          continue;
+          }
+          
+          System.out.println("Wynik: "+ (a/b));
           break;
       }
     }
